576c50d3699fe73c96a23cdd44c0c07ff26465d7
Ivan Krakovskii <iv.krak@gmail.com>
2024-04-30 00:57:35 +0800

    Пытаемся запустить это...

--- pmtestpayment.py
+++ pmtestpayment.py
@@ -55,5 +55,24 @@ class TestPaymentModule(payment.PaymentModule):
             logger.info(f"change status for payment {p['id']}")
             payment.set_paid(p['id'], '', f"external_{p['id']}")
 
+print('''
+       _,met$$$$$gg.          bot@cogent-sidewalk.aeza.network
+    ,g$$$$$$$$$$$$$$$P.       --------------------------------
+  ,g$$P"     """Y$$.".        OS: Debian GNU/Linux 12 (bookworm) x86_64
+ ,$$P'              `$$$.     Host: KVM RHEL 7.6.0 PC (i440FX + PIIX, 1996)
+',$$P       ,ggs.     `$$b:   Kernel: 6.1.0-18-amd64
+`d$$'     ,$P"'   .    $$$    Uptime: 4 days, 13 hours, 20 mins
+ $$P      d$'     ,    $$P    Packages: 492 (dpkg)
+ $$:      $$.   -    ,d$$'    Shell: zsh 5.9
+ $$;      Y$b._   _,d$P'      Resolution: 1024x768
+ Y$$.    `.`"Y$$$$P"'         Terminal: /dev/pts/3
+ `$$b      "-.__              CPU: AMD Ryzen 9 7950X3D (1) @ 4.192GHz
+  `Y$$                        GPU: 00:02.0 Cirrus Logic GD 5446
+   `Y$$.                      Memory: 926MiB / 1967MiB
+     `$$b.
+       `Y$$b.
+          `"Y$b._
+              `"""
+''')
 print(f'{__name__} запущен')
 TestPaymentModule().Process()
